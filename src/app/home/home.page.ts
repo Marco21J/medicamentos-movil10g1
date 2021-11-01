219e1d5c2cb5f1eea6f896b670bae15f292e9796
Oscar Reyes <reyesmartinezoscar3g15@gmail.com>
2021-11-01 15:50:50 -0600

    Segundo commit de prueba

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HomeService } from './services/home.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,12 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public text = '';
+
+  constructor(private homeService: HomeService) {}
+
+  public onClick(): void {
+    this.text = this.homeService.testMethod();
+  }
 
 }
---
+++ src/app/home/services/home.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HomeService {
+
+  constructor() { }
+
+  public testMethod(): string {
+    return 'Hello world from Ionic, I am a method from home service';
+  }
+}
